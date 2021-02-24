eb8de97832cc462cbdb502c83d888927a5323612
Jason Kibblewhite <jkibble@gmail.com>
2021-02-23 22:26:41 -0700

    create flask todo app with templates and partials

--- stonks/__init__.py
+++ stonks/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
-from dotenv import load_dotenv
-load_dotenv()
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr(self):
+          return f'<Task {self.id}'
 
 print(f'{__name__} starting')
 
--- stonks/views.py
+++ stonks/views.py
@@ -1,6 +1,40 @@
-from stonks import app
-from flask import render_template
+from stonks import app, db, Todo
+from flask import render_template, request, redirect
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        content = request.form['content']
+        task = Todo(content=content)
+
+        try:
+            db.session.add(task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Failed to delete'
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    task.content = request.form['content']
+
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error updating task'
